e7d6f745680fc7fd1b50aa913fe024d96c592a9b
Gbemmiey <gbemfemalabi@gmail.com>
2021-04-30 22:33:17 +0100

    Completed

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var txtBtn: Button
+    private lateinit var txtVw: TextView
+
+    var displayText: Int = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        txtVw = findViewById(R.id.textView)
+        txtBtn = findViewById(R.id.incrBtn)
+        txtBtn.setOnClickListener(){increment()}
     }
+
+    fun increment() {
+        displayText += 1
+        txtVw.text = displayText.toString()
+    }
+
 }
